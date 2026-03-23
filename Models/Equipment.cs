f66e34d28c0ac054c65b1d66697d0b42738b48e8
s30697 <s30697@pjwstk.edu.pl>
2026-03-23 14:26:28 +0100

    Add abstract class for Equipment with ID and status logic

---
+++ Models/Equipment.cs
@@ -0,0 +1,25 @@
+﻿namespace UczelnianaWypozyczalnia.Models;
+
+    public abstract class Equipment
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; set; }
+        public bool IsAvailable { get; protected set; }
+
+        protected Equipment(string name)
+        {
+            Id = Guid.NewGuid(); 
+            Name = name;
+            IsAvailable = true;
+        }
+
+        public void MarkAsUnavailable()
+        {
+            IsAvailable = false;
+        }
+
+        public void MarkAsAvailable()
+        {
+            IsAvailable = true;
+        }
+    }
